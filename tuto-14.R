1b896adc91b0fcedf296a35ee24f4206a9b45bdf
lgsp <laurent.garnier.superprof@gmail.com>
2021-07-23 19:31:42 +0300

    Works with matrices and plot

---
+++ tuto-14.R
@@ -0,0 +1,15 @@
+
+
+
+Games
+rownames(Games)
+colnames(Games)
+Games["LeBronJames", "2012"]
+
+FieldGoals
+
+FieldGoals / Games
+round(FieldGoals / Games, 1)
+
+MinutesPlayed / Games
+round(MinutesPlayed / Games, 1)
